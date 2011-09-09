a524db9ab55eae286bd3382ef2c17644dfa71f09
Vinicius Depizzol <vdepizzol@gmail.com>
2011-09-09 02:46:13 -0300

    behavior for global domain bar user menu

--- template.js
+++ template.js
@@ -1,4 +1,47 @@
 jQuery(function() {
+    
+    /* Global Domain bar user area */
+    
+    jQuery('body').click(function(e) {
+        
+        global_domain_bar_user_settings('close');
+        
+    });
+    
+    function global_domain_bar_user_settings(action) {
+        
+        if (action == 'open') {
+            
+            jQuery('#global_domain_bar .user').addClass('active');
+            
+            jQuery('#global_domain_bar .user_settings').show().css({top: '-10px', opacity: 0}).animate({top: '0', opacity: 1}, 200);
+            
+        } else if (action == 'close') {
+            
+            jQuery('#global_domain_bar .user').removeClass('active');
+            
+            jQuery('#global_domain_bar .user_settings').animate({top: '10px', opacity: 0}, 200, function() {
+                jQuery(this).hide();
+            });
+        }
+        
+    }
+    
+    jQuery('#global_domain_bar .user').click(function(e) {
+        
+        e.preventDefault();
+        
+        e.stopPropagation();
+        
+        if (!jQuery(this).hasClass('active')) {
+            global_domain_bar_user_settings('open');
+        } else {
+            global_domain_bar_user_settings('close');
+        }
+        
+    });
+    
+    /* What's new textarea behavior */
 
     jQuery('#whats-new').click(function(e) {
     
