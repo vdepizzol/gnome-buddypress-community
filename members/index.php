24f803e6c422aa87e3678200608ec6f9b1672e60
Vinicius Depizzol <vdepizzol@gmail.com>
2011-09-09 02:41:12 -0300

    new pin icon and code consistency for map render in members directory

--- members/index.php
+++ members/index.php
@@ -1,67 +1,71 @@
-<?php
-
-global $bp;
-
-delete_transient('gnome_all_users_position');
-
-if (false === ($users_on_map = get_transient('gnome_all_users_position'))) {
-
-    $users_on_map = $wpdb->get_results( "SELECT `value`, `user_id` FROM " . $bp->profile->table_name_data . " " .
-                                        "WHERE `field_id` = (SELECT `id` FROM ". $bp->profile->table_name_fields ." ".
-                                                            "WHERE `name` = '" . mysql_real_escape_string(GNOME_FIELD_GEOPOSITION) . "')");
-                                                            
-    set_transient('gnome_all_users_position', serialize($users_on_map), (3600 * 60 * 1));
-    
-} else {
-    
-    $users_on_map = unserialize($users_on_map);
-
-}
-
-
-?>
 <?php get_header() ?>
 
 	<div id="content">
 		<div class="padder">
 
 		<form action="" method="post" id="members-directory-form" class="dir-form">
-		
-			<div id="members-map" style="height: 400px; margin-bottom: 20px;">
-		    </div>
-
-			<h3><?php _e( 'Members Directory', 'buddypress' ) ?></h3>
-
-			<?php do_action( 'bp_before_directory_members_content' ) ?>
-		    
-		    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/map/leaflet.js"></script>
-            <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/map/leaflet.css" />
             
-            <script type="text/javascript">
-                var map = new L.Map('members-map');
+            <?php if (!isset($_GET['s']) && (!isset($_GET['upage']) || $_GET['upage'] == '1')): ?>
+		
+                <div id="members-map" style="height: 400px; margin-bottom: 20px;">
+                </div>
+                            
+                <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/map/leaflet.js"></script>
+                <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/map/leaflet.css" />
+                
+                <script type="text/javascript">
+                    var map = new L.Map('members-map');
 
-                var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/42030/256/{z}/{x}/{y}.png',
-                    cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-                    cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom:4, attribution: cloudmadeAttribution});
-                
-                map.setView(new L.LatLng(15, 0), 2).addLayer(cloudmade);
-                
-                <?php
-                
-                foreach ($users_on_map as $id => $user) {
-                
-                    $latlng = explode('/', $user->value);
-			        $lat = number_format($latlng[0], 1, '.', '');
-			        $lng = number_format($latlng[1], 1, '.', '');
-			        
-                    ?>var marker<?php echo $id;?> = new L.Marker(new L.LatLng(<?php echo $lat;?>,<?php echo $lng;?>));map.addLayer(marker<?php echo $id;?>);<?php
-                }
-                
-                ?>
+                    var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/42030/256/{z}/{x}/{y}.png',
+                        cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
+                        cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom:8, attribution: cloudmadeAttribution});
+                    
+                    map.setView(new L.LatLng(15, 0), 2).addLayer(cloudmade);
+                    
+                    var pinIcon = L.Icon.extend({
+                        iconUrl: '<?php bloginfo('stylesheet_directory'); ?>/map/images/pin.png',
+                        shadowUrl: '<?php bloginfo('stylesheet_directory'); ?>/map/images/pin-shadow.png',
+                        iconSize: new L.Point(12, 28),
+                        shadowSize: new L.Point(20, 28),
+                        iconAnchor: new L.Point(6, 24),
+                        popupAnchor: new L.Point(0, -24)
+                    });
+                    var pin = new pinIcon();
+                    
+                    var members = jQuery.ajax({
+                        url: '?members_json',
+                        dataType: 'json',
+                        success: function(data) {
+                            var marker = {};
+                            jQuery.each(data, function(index, value) {
+                                marker[index] = new L.Marker(new L.LatLng(parseFloat(value.lat), parseFloat(value.lng)), {icon: pin});
+                                marker[index].bindPopup("<a class=\"user_tooltip\" href=\"<?php echo site_url() ?>/members/" + value.user + "/\"><b>" + value.name + "</b><br />" + value.location + '</a>');
+                                map.addLayer(marker[index]);
+                            });
+                        }
+                    });
 
-                map.addLayer(marker);
-
-            </script>
+                </script>
+                
+                <hr class="top_shadow" />
+                
+                <h3 style="text-align: center;">GNOME is a diverse international community</h3>
+                
+                <p class="main_feature" style="text-align: center;">We are programmers, designers, translators and writers from all<br /> over the world with the same vision.</p>
+            
+            <?php else: ?>
+            
+            <div class="page_title">
+            <?php if (isset($_GET['s'])): ?>
+                <h1>Looking for <em><?php echo htmlspecialchars(strip_tags($_GET['s']));?></em>...</h1>
+            <?php else: ?>
+                <h1>Members directory</h1>
+            <?php endif; ?>
+            </div>
+            
+            <?php endif; ?>
+            
+			<?php do_action( 'bp_before_directory_members_content' ) ?>
 
 			<div id="members-dir-list" class="members dir-list">
 				<?php locate_template( array( 'members/members-loop.php' ), true ) ?>
--- members/members-loop.php
+++ members/members-loop.php
@@ -1,8 +1,6 @@
-<?php /* Querystring is set via AJAX in _inc/ajax.php - bp_dtheme_object_filter() */ ?>
-
 <?php do_action( 'bp_before_members_loop' ) ?>
 
-<?php if ( bp_has_members( bp_ajax_querystring( 'members' ) ) ) : ?>
+<?php if ( bp_has_members( 'per_page=15' ) ) : ?>
 
 	<?php do_action( 'bp_before_directory_members_list' ) ?>
 
@@ -10,43 +8,45 @@
 	<?php while ( bp_members() ) : bp_the_member(); ?>
 
 		<li>
-			<div class="item-avatar">
-				<a href="<?php bp_member_permalink() ?>"><?php bp_member_avatar() ?></a>
-			</div>
+            <a href="<?php bp_member_permalink() ?>">
+                <span class="item-avatar">
+                    <?php bp_member_avatar(array('alt' => '')) ?>
+                </span>
 
-			<div class="item">
-				<div class="item-title">
-					<a href="<?php bp_member_permalink() ?>"><?php bp_member_name() ?></a>
-				</div>
+                <span class="item">
+                    <span class="item-title">
+                        <?php bp_member_name() ?>
+                    </span>
 
-				<div class="item-meta">
-				    <?php bp_profile_field_data( array('field' => GNOME_FIELD_LOCATION, 'user_id' => bp_get_member_user_id() ) ); ?>
-				</div>
+                    <span class="item-meta">
+                        <?php bp_profile_field_data( array('field' => GNOME_FIELD_LOCATION, 'user_id' => bp_get_member_user_id() ) ); ?>
+                    </span>
 
-				<?php do_action( 'bp_directory_members_item' ) ?>
-			</div>
+                    <?php do_action( 'bp_directory_members_item' ) ?>
+                </span>
 
-			<div class="action">
+                <span class="action">
 
-				<?php do_action( 'bp_directory_members_actions' ); ?>
+                    <?php do_action( 'bp_directory_members_actions' ); ?>
 
-			</div>
-
-			<div class="clear"></div>
+                </span>
+            </a>
 		</li>
 
 	<?php endwhile; ?>
 	</ul>
 
 	<?php do_action( 'bp_after_directory_members_list' ) ?>
+    
+    <div class="clear"></div>
+    
+    <?php if (bp_get_members_pagination_links() != '') { ?>
+    <hr class="bottom_shadow" />
+    <?php } ?>
 
 	<?php bp_member_hidden_fields() ?>
 
 	<div id="pag-bottom" class="pagination">
-
-		<div class="pag-count" id="member-dir-count-bottom">
-			<?php bp_members_pagination_count() ?>
-		</div>
 
 		<div class="pagination-links" id="member-dir-pag-bottom">
 			<?php bp_members_pagination_links() ?>
@@ -56,9 +56,21 @@
 
 <?php else: ?>
 
-	<div id="message" class="info">
-		<p><?php _e( "Sorry, no members were found.", 'buddypress' ) ?></p>
-	</div>
+    <div class="content" style="padding: 0 80px">
+        
+        <h2>Sorry, but no one was found.</h2>
+        
+        <p>Suggestions:</p>
+        
+        <ul>
+            <li>Make sure all words are spelled correctly.</li>
+            <li>Try different keywords.</li>
+            <li>Try fewer keywords.</li>
+        </ul>
+        
+        <p>If you feel lost, you may want to search for <?php echo htmlspecialchars(stripslashes(strip_tags($_GET['s'])));?> in all GNOME websites on <a href="http://google.com/search?q=<?php echo htmlspecialchars(stripslashes(strip_tags($_GET['s'])));?>%20site:gnome.org">Google</a>.</p>
+    
+    </div>
 
 <?php endif; ?>
 
